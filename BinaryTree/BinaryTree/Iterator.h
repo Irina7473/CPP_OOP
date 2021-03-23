fcaa8d125ef7b092e358b0f5f2507e1d53f5dcde
Irina <ten7473@mail.ru>
2021-03-23 20:48:26 +0500

    temp4

--- BinaryTree/BinaryTree/Iterator.h
+++ BinaryTree/BinaryTree/Iterator.h
@@ -11,20 +11,6 @@ public:
 	const int& operator*()const { return Temp->Data; };
 	int& operator*() { return Temp->Data; };
 
-	Iterator& operator ++()
-	{
-		//return (Temp->pRight) ? maxValue(Root->pRight) : Root->Data;
-
-		//(Temp->pLeft)? Temp = Temp->pLeft : Temp = Temp->pRight;
-		return *this;
-	}
-	Iterator operator ++(int)
-	{
-		Iterator old = *this;
-		(Temp) ? Temp = Temp->pLeft : Temp = Temp->pRight;
-		return old;
-	}
-
 	bool operator==(const Iterator& other)const
 	{return this->Temp == other.Temp;}
 	bool operator!=(const Iterator& other)const
